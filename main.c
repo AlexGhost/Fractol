052fbae76bd6fced10c5f9c451bb0b247a1bacae
acourtin <acourtin@student.42.fr>
2017-12-17 19:03:21 +0100

    added modes for different fractol

--- main.c
+++ main.c
@@ -6,15 +6,33 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 11:34:34 by acourtin          #+#    #+#             */
-/*   Updated: 2017/12/17 15:37:58 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/12/17 19:02:35 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int			main(void)
+static void		write_usage(void)
 {
-	ft_putendl("--- Welcome to Fractolol ---");
-	fr_create_window();
+	ft_putendl("usage: ./fractol <mode>");
+	ft_putendl("\tmode 1 : Mandelbrot");
+	exit(0);
+}
+
+int				main(int ac, char **av)
+{
+	int mode;
+
+	if (ac != 2)
+		write_usage();
+	else
+		mode = ft_atoi(av[1]);
+	if (mode != 1)
+		write_usage();
+	else
+	{
+		ft_putendl("--- Welcome to Fractolol ---");
+		fr_create_window();
+	}
 	return (0);
 }
